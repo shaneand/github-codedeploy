7fda2f3c2996d7a66c21084daf1f93a33e020d9a
Shane Anderson <sanderson72@gmail.com>
2017-12-31 09:50:06 -0500

    update mod-index cp command

--- scripts/mod-index.sh
+++ scripts/mod-index.sh
@@ -3,5 +3,5 @@
 hname=`hostname`
 cp -p /var/www/html/index.html /var/www/html/index.html.orig
 cat /var/www/html/index.html | sed -e "s/HOSTNAME/${hname}/g" > /var/www/html/index.html.new
-cp -p /var/www/html/index.html.new /var/www/html/index.html
+cp /var/www/html/index.html.new /var/www/html/index.html
 
