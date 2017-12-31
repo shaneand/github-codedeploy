77feb0b9140cfe5edb7c010b412d5024ec163797
Shane Anderson <sanderson72@gmail.com>
2017-12-31 10:23:28 -0500

    add curl to mod-index.sh

--- scripts/mod-index.sh
+++ scripts/mod-index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hname=`hostname`
+hname=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 cp -p /var/www/html/index.html /var/www/html/index.html.orig
 cat /var/www/html/index.html | sed -e "s/HOSTNAME/${hname}/g" > /var/www/html/index.html.new
 cp /var/www/html/index.html.new /var/www/html/index.html
