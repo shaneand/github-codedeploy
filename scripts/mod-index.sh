e6b59f15d79719c8a429695c7c58d1ab35415004
Shane Anderson <sanderson72@gmail.com>
2017-12-31 09:46:36 -0500

    update linuxapp.tar mod-index.sh

--- scripts/mod-index.sh
+++ scripts/mod-index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hname = `hostname`
+hname=`hostname`
 cp -p /var/www/html/index.html /var/www/html/index.html.orig
 cat /var/www/html/index.html | sed -e "s/HOSTNAME/${hname}/g" > /var/www/html/index.html.new
 cp -p /var/www/html/index.html.new /var/www/html/index.html
